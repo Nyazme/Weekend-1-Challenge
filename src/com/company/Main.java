80e267d1eab1cd712472ab703868cb58d1584d5c
Mark <feinmanmarke@gmail.com>
2018-01-14 23:06:01 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Create a calculator! One that can add, subtract, multiply, and divide.
+        //First ask the user to enter a number.
+        //Then ask them what operator they want to use ("+", "-", "*", or "/").
+        //Finally, ask them to enter a second number.write your code here
+        System.out.println("Hello Math Genius!, lets use a Calculator");
+        Scanner Calculator = new Scanner(System.in);
+        String inputFromMathGenius;
+
+    }
+
+        //System.out.println(myNumber);
+        //System.out.println(7);
+        //int myNumber = 123 * 456;
+        //int sum = 34 + 113;
+        //int difference = 91 - 205;
+        //int product = 2 * 8;
+        //int quotient = 45 / 3;
+        //System.out.println(2 > 1);
+
+        }
+
+
+
