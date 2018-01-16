7fbad796c8f7263c5c5335d573062d79e41ffc16
Mark <feinmanmarke@gmail.com>
2018-01-15 20:23:12 -0700

    2nd revision with errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static String inputFromUser;
+    int convertedNumber = Integer.parseInt(inputFromUser);
+
+
+
+
     public static void main(String[] args) {
 	// Create a calculator! One that can add, subtract, multiply, and divide.
         //First ask the user to enter a number.
         //Then ask them what operator they want to use ("+", "-", "*", or "/").
         //Finally, ask them to enter a second number.write your code here
-        System.out.println("Hello Math Genius!, lets use a Calculator");
-        Scanner Calculator = new Scanner(System.in);
-        String inputFromMathGenius;
+        System.out.println("Hello Math Genius!, Please enter a Number");
+        Scanner sc = new Scanner(System.in);
+        String inputFromMathGenius; sc. nextLine();
+        System.out.println("convertedNumber");
+        System.out.println("Converted Number + 5");
+
+        (int i = 0; i < questions.length, i++);
+        Scanner sc = newScanner(System.in);
+        System.out.println(questions[i]);
+        sc.nextLine();
+        answers.add (userEntry);
+
 
     }
 
@@ -24,7 +39,7 @@ public class Main {
         //int quotient = 45 / 3;
         //System.out.println(2 > 1);
 
-        }
+
 
 
 
